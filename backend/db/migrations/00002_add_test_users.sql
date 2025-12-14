69c0244b9685839dc4a948239579b3b5c87e96bc
Alexandr Timchenko <atimchenko92@gmail.com>
2025-12-14 15:51:16 +0100

    fix: migration logic error
    Fix possible migration logic error, because of email UNIQUE property.

--- backend/db/migrations/00002_add_test_users.sql
+++ backend/db/migrations/00002_add_test_users.sql
@@ -4,7 +4,7 @@ VALUES
     (1, 'user1@example.com', '$2a$10$iehMYy8l.nvHJ.CxRqmtJOhfRJHw1kpnQfHDmJX7Qorq1QVeI.1lK', 'Testo', 'Matesto', 10000.0, NOW()), -- PW: test123
     (2, 'user2@example.com', '$2a$10$iehMYy8l.nvHJ.CxRqmtJOhfRJHw1kpnQfHDmJX7Qorq1QVeI.1lK', 'Loba', 'Boba', 5000.0, NOW()), -- PW: test123
     (3, 'user3@example.com', '$2a$10$iehMYy8l.nvHJ.CxRqmtJOhfRJHw1kpnQfHDmJX7Qorq1QVeI.1lK', 'Tian', 'Petyan', 10000.0, NOW()) -- PW: test123
-ON CONFLICT (id) DO NOTHING;
+ON CONFLICT (email) DO NOTHING;
 
 SELECT setval('users_id_seq', (SELECT MAX(id) FROM users));
 
