8b7b7fa99fca77eff8df091888352b4514f3f91c
Alexandr Timchenko <atimchenko92@gmail.com>
2026-01-27 00:53:06 +0100

    chore: added formatter/linter ignore file
    Exclude pathes like the ones for protobufs

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -6,7 +6,7 @@ import tseslint from 'typescript-eslint';
 import { defineConfig, globalIgnores } from 'eslint/config';
 
 export default defineConfig([
-  globalIgnores(['dist']),
+  { ignores: ['dist', 'src/lib/proto'] },
   {
     files: ['**/*.{ts,tsx}'],
     extends: [
