71b5049e621698a14224248df21b7043f57a9b7c
Alexandr Timchenko <atimchenko92@gmail.com>
2026-01-26 21:43:35 +0100

    feat(frontend): prepare vite to handle cookie requests

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -8,16 +8,23 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '../', '')
   const exchangePort = parseInt(env.SERVER_PORT) || 8081
   const clientPort = parseInt(env.CLIENT_PORT) || 5173
-  const apiUrl = `http://localhost:${exchangePort}/api`
 
   return {
     plugins: [react()],
     define: {
-      'import.meta.env.VITE_API_URL': JSON.stringify(apiUrl),
+      'import.meta.env.VITE_API_URL': JSON.stringify('/api'),
     },
     server: {
       port: clientPort,
       host: true,
+      // Forward requests to backend (avoid CORS hell)
+      proxy: {
+        '/api': {
+          target: `http://localhost:${exchangePort}`,
+          changeOrigin: true,
+          secure: false,
+        },
+      },
     },
     test: {
       globals: true,
