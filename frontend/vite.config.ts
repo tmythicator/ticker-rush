c9fabd648e232847b9505461804a61cc8bd7f4fb
Alexandr Timchenko <atimchenko92@gmail.com>
2026-02-10 12:40:57 +0100

    config(frontend): add path alias

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,3 +1,4 @@
+import path from "path";
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
@@ -11,6 +12,11 @@ export default defineConfig(({ mode }) => {
 
   return {
     plugins: [react()],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
+    },
     esbuild: {
       drop: mode === 'production' ? ['console', 'debugger'] : [],
     },
